14ce6d25597ef0d359bf6b32b8937eaa1dbabd13
Anagha <anaghalakshmeeka@gmail.com>
2023-12-05 10:21:34 +0530

    test-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [num, setnum] = useState(0)
+  const [sign, setsign] = useState('positive')
+  
+  function checkSign(n){
+    n>=0?setsign('positive'):setsign('negative');
+  }
+
+  function increment() {
+    setnum(num + 1)
+    checkSign(num+1)  ///important point to note
+  }
+  function decrement() {
+    setnum(num - 1)
+    checkSign(num-1)
+  }
+  function reset() {
+    setnum(0)
+    checkSign(0)
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='counter' >
+      <h2 className='fw-light mt-3 mb-0' >
+          COUNTER
+        </h2>
+        <h1 className={sign}> {num} </h1>
+        <span onClick={decrement} className="material-symbols-outlined">
+          do_not_disturb_on
+        </span>
+        <span onClick={increment} className="material-symbols-outlined">
+          add_circle
+        </span>
+        <span onClick={reset} className="material-symbols-outlined">
+          refresh
+        </span>
+      </div>
     </div>
   );
 }
